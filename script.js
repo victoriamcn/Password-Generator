e0eeb3e86a5aa21abb56c1e9132957a09b220f00
Victoria <vmcnorrill@gmail.com>
2023-02-08 20:31:35 -0500

    started JS code. created a variable for characters, minimum characters allowed and maximum characters allowed. started conditional statement with a for loop to accept or deny pw. now realizing that I need the application to generate this PW, not the user

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//code to say that only these characters permitted: !@#$%^&*()=+[{]}<\>/?
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  //CHARACTERS GENERATED , MINIMUM 8 , MAXIMUM 128 - should the chosen variable be passwordText or password?
+  function generatePassword() {
+    var charSet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*()=+[]{}}<\>/?"
+    var passwordMinLength = 8;
+    var passwordMaxlength = 128;
+      //conditional statement for loop to check PW length
+      if i>=passwordMinLength && i<=passwordMaxLength {for (var i = 0, i>=passwordMinLength; i++){
+        var randomPW =Math.floor(Math.random() * charSet);
+        passwordText
+      }
+      } if else  {
+        alert("Please write a password that is between " + passwordMinLength + " and " + passwordMaxLength + ".");
+        return false;
+      } else {
+        alert ("You have met the criteria.")
+        return true;
+      }
+        
+      }
 
+  //.value specifies the value of the input text field
+  passwordText.value = password;
 }
 
 // Add event listener to generate button
