71436e42170c807d135c264edafa6ef919923954
Victoria <vmcnorrill@gmail.com>
2023-02-11 01:01:45 -0500

    changed getRandom from functions to variables because my code still isn't working

--- script.js
+++ script.js
@@ -3,8 +3,7 @@
 //Button DOM Selector
 let generateBtn = document.querySelector("#generate");
 //User selection and confirm variables
-let userLength; //Variable for user's password length ~line 50
-let promptLength; //made promptLength for the prompt window asking for PW length
+let input; //Variable for user's password length ~line 50
 let confirmUppercase; //made confirm for each character set so I could list all possible selections in my conditional statements ~line66
 let confirmLowercase;
 let confirmNumber;
@@ -27,14 +26,11 @@ let number = [1,2,3,4,5,6,7,8,9,0];
 let specialChar = ["!","@","#","$","%","^","&","*","(",")","=","+","[","{","]","}","<",">","/","?"];
   //console.log(specialChar);
 
-//Function for getting a random element from an array//function getRandom(uppercase) {// var randUpperCase = Math.floor(Math.random() * upperCase.length);//var randElement = arr[randUpperCase];return randElement;}
-
 // Write password to the #password input
 function writePassword() {
   let password = createPassword();
   let passwordText = document.querySelector("#password");
   //tried created a new element to put the new password in and it didn't work//var elementP = document.createElement("p");//elementP.textContent = password;//console.log(elementP)
-  
   //Password value into the text box
   passwordText.value = password;
 }
@@ -45,102 +41,78 @@ generateBtn.addEventListener("click", writePassword); //debugger
 
 //Function to Generate PW
 function createPassword() {
-  
-  promptLength = parseInt(prompt("How long would you like your password to be? Please type a number between 8 and 128."));
+  input = parseInt(prompt("How long would you like your password to be? Please type a number between 8 and 128."));
   //validate entry //parseInt converts first argument to string, parses it, then returns an integer or NaN 
-  if (userLength < 8 || userLength > 128) {
-    Event.preventDefault();
-    Event.stopPropagation();     // window returns to the alert if user make invalid length choice
-    parseInt(alert("Please type a number between 8 and 128."));
-  } else {
-    confirmUppercase = confirm("Would you like your password to have UPPERCASE letters?");
-    confirmLowercase = confirm("would you like your password to have lowercase letters?");
-    confirmNumber = confirm("Would you like your password to have numbers?");
-    confirmSpecialChar = confirm("Would you like your password to contain special characters?: ! @ # $ % & * = + < > / ?");
-};
+  if (input < 8 || input > 128) {
+      alert("Please type a number between 8 and 128.");
+    } else {
+      confirmUppercase = confirm("Would you like your password to have UPPERCASE letters?");
+      confirmLowercase = confirm("would you like your password to have lowercase letters?");
+      confirmNumber = confirm("Would you like your password to have numbers?");
+      confirmSpecialChar = confirm("Would you like your password to contain special characters?: ! @ # $ % & * = + < > / ?");
+    }
 
-// User's Character Selection // Found that concat works best rather than push.
-    // No selections made
-if (!confirmUppercase && !confirmLowercase && !confirmNumber && !confirmSpecialChar) {
-  alert("My dude, you need to have a password. Please choose 'Okay' or 'Cancel' from the criteria.");
-  userSelection = " ";
-              // All selections made
-  } else if (confirmUppercase && confirmLowercase && confirmNumber && confirmSpecialChar) {
-    userSelection.concat(getRandomUpper,getRandomLower,getRandomNumber,getRandomSpecialChar);
+  // User's Character Selection // advised .concat works best rather than push.
+                // All selections made
+    if (confirmUppercase && confirmLowercase && confirmNumber && confirmSpecialChar) {
+      userSelection.concat(getRandomUpper,getRandomLower,getRandomNumber,getRandomSpecialChar);
+      //console.log(userSelection);
+                // 3/4 Selections Made !upperCase
+    } else if (!confirmUppercase && confirmLowercase && confirmNumber && confirmSpecialChar) {
+      userSelection.concat(getRandomLower,getRandomNumber,getRandomSpecialChar);
+                // 3/4 Selections Made !lowerCase
+    } else if (confirmUppercase && !confirmLowercase && confirmNumber && confirmSpecialChar) {
+      userSelection.concat(getRandomUpper,getRandomNumber,getRandomSpecialChar);
+                // 3/4 Selections Made !number
+    } else if (confirmUppercase && confirmLowercase && !confirmNumber && confirmSpecialChar) {
+      userSelection.concat(getRandomUpper,getRandomLower,getRandomSpecialChar);
+                // 3/4 Selections Made !specialChar
+    } else if (confirmUppercase && confirmLowercase && confirmNumber && !confirmSpecialChar) {
+      userSelection.concat(getRandomUpper,getRandomLower,getRandomNumber);
+                // 2/4 Selections Made !upperCase !lowerCase
+    } else if (!confirmUppercase && !confirmLowercase && confirmNumber && confirmSpecialChar) {
+      userSelection.concat(getRandomNumber,getRandomSpecialChar);
+                // 2/4 Selections Made !upperCase !number
+    } else if (!confirmUppercase && confirmLowercase && !confirmNumber && confirmSpecialChar) {
+      userSelection.concat(getRandomLower,getRandomSpecialChar);
+                // 2/4 Selections Made !upperCase !specialChar
+    } else if (!confirmUppercase && confirmLowercase && confirmNumber && !confirmSpecialChar) {
+      userSelection.concat(getRandomLower,getRandomNumber);
+                // 2/4 Selections Made !lowerCase !number
+    } else if (confirmUppercase && !confirmLowercase && !confirmNumber && confirmSpecialChar) {
+      userSelection.concat(getRandomUpper,getRandomSpecialChar);
+                // 2/4 Selections Made !lowerCase !specialChar
+    } else if (confirmUppercase && !confirmLowercase && confirmNumber && !confirmSpecialChar) {
+      userSelection.concat(getRandomUpper,getRandomNumber);
+                // 2/4 Selections Made !number !specialChar
+    } else if (confirmUppercase && confirmLowercase && !confirmNumber && !confirmSpecialChar) {
+      userSelection.concat(getRandomUpper,getRandomLower);
+                // 1/4 Selections Made for upperCase only
+    //don't need .concat here because it's only one confirm
+    } else if (confirmUppercase && !confirmLowercase && !confirmNumber && !confirmSpecialChar) {
+      userSelection = getRandomUpper;
+                // 1/4 Selections Made for lowerCase only
+    } else if (!confirmUppercase && confirmLowercase && !confirmNumber && !confirmSpecialChar) {
+      userSelection =  getRandomLower;
+                // 1/4 Selections Made for number only 
+    } else if (!confirmUppercase && !confirmLowercase && confirmNumber && !confirmSpecialChar) {
+      userSelection =  getRandomNumber;
+                // 1/4 Selections Made for specialChar only
+    } else if (!confirmUppercase && !confirmLowercase && !confirmNumber && confirmSpecialChar) {
+      userSelection =  getRandomSpecialChar;
+      // No selections made
+  } else (!confirmUppercase && !confirmLowercase && !confirmNumber && !confirmSpecialChar) {
+    alert("Please choose from the accepted criteria.");
 
-    //console.log(userSelection);
-
-              // 3/4 Selections Made !upperCase
-  } else if (!confirmUppercase && confirmLowercase && confirmNumber && confirmSpecialChar) {
-    userSelection.concat(getRandomLower,getRandomNumber,getRandomSpecialChar);
-              // 3/4 Selections Made !lowerCase
-  } else if (confirmUppercase && !confirmLowercase && confirmNumber && confirmSpecialChar) {
-    userSelection.concat(getRandomUpper,getRandomNumber,getRandomSpecialChar);
-              // 3/4 Selections Made !number
-  } else if (confirmUppercase && confirmLowercase && !confirmNumber && confirmSpecialChar) {
-    userSelection.concat(getRandomUpper,getRandomLower,getRandomSpecialChar);
-              // 3/4 Selections Made !specialChar
-  } else if (confirmUppercase && confirmLowercase && confirmNumber && !confirmSpecialChar) {
-    userSelection.concat(getRandomUpper,getRandomLower,getRandomNumber);
-
-              // 2/4 Selections Made !upperCase !lowerCase
-  } else if (!confirmUppercase && !confirmLowercase && confirmNumber && confirmSpecialChar) {
-    userSelection.concat(getRandomNumber,getRandomSpecialChar);
-              // 2/4 Selections Made !upperCase !number
-  } else if (!confirmUppercase && confirmLowercase && !confirmNumber && confirmSpecialChar) {
-    userSelection.concat(getRandomLower,getRandomSpecialChar);
-              // 2/4 Selections Made !upperCase !specialChar
-  } else if (!confirmUppercase && confirmLowercase && confirmNumber && !confirmSpecialChar) {
-    userSelection.concat(getRandomLower,getRandomNumber);
-              // 2/4 Selections Made !lowerCase !number
-  } else if (confirmUppercase && !confirmLowercase && !confirmNumber && confirmSpecialChar) {
-    userSelection.concat(getRandomUpper,getRandomSpecialChar);
-              // 2/4 Selections Made !lowerCase !specialChar
-  } else if (confirmUppercase && !confirmLowercase && confirmNumber && !confirmSpecialChar) {
-    userSelection.concat(getRandomUpper,getRandomNumber);
-              // 2/4 Selections Made !number !specialChar
-  } else if (confirmUppercase && confirmLowercase && !confirmNumber && !confirmSpecialChar) {
-    userSelection.concat(getRandomUpper,getRandomLower);
-
-              // 1/4 Selections Made for upperCase only
-  //don't need .concat here because it's only one confirm
-  } else if (confirmUppercase && !confirmLowercase && !confirmNumber && !confirmSpecialChar) {
-    userSelection = getRandomUpper;
-              // 1/4 Selections Made for lowerCase only
-  } else if (!confirmUppercase && confirmLowercase && !confirmNumber && !confirmSpecialChar) {
-    userSelection =  getRandomLower;
-              // 1/4 Selections Made for number only 
-  } else if (!confirmUppercase && !confirmLowercase && confirmNumber && !confirmSpecialChar) {
-    userSelection =  getRandomNumber;
-              // 1/4 Selections Made for specialChar only
-  } else if (!confirmUppercase && !confirmLowercase && !confirmNumber && confirmSpecialChar) {
-    userSelection =  getRandomSpecialChar;
-  };
-
-  //below are the loops and functions for the random selection of characters based on 
-    //functions for getting random characters for each set
-    function getRandomUpper() {
-      Math.floor(Math.random() * upperCase.length);
-    };
-
-    function getRandomLower() {  
-      Math.floor(Math.random() * lowerArr.length);
-    };
-
-    function getRandomNumber() {
-      Math.floor(Math.random() * number.length);
-    };
-
-    function getRandomSpecialChar() {
-      Math.floor(Math.random() * specialChar.length);
-    };
+  let getRandomUpper = [Math.floor(Math.random() * upperCase.length)];
+  let getRandomLower= [Math.floor(Math.random() * lowerCase.length)]; 
+  let getRandomNumber = [Math.floor(Math.random() * number.length)];
+  let getRandomSpecialChar = [Math.floor(Math.random() * specialChar.length)];
 
     //for loop selecting random characters based on chosen length
     for (let i = 0; i < userLength; i++) {
-      var passwordPush = userSelection[Math.floor(Mat.random() * userSelection.length)]
-      password.push(passwordPush);
-      //didn't work, put the getRandom functions in the confirm instead.. .concat(getRandomUpper,getRandomLower,getRandomNumber,getRandomSpecialChar);
-  };
+      let generatedPassword = " "
+      generatedPassword.slice(userSelection);
+    } //didn't work, put the getRandom functions in the confirm instead.. .concat(getRandomUpper,getRandomLower,getRandomNumber,getRandomSpecialChar);
 
-return password
-};
+}
