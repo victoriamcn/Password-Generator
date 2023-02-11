47edd0d2c71ead123d6ae5a4c033dc6610dacfcf
Victoria <vmcnorrill@gmail.com>
2023-02-11 17:13:36 -0500

    added credits to README. simplified JS code. still struggling with the for loop and return

--- README.md
+++ README.md
@@ -3,7 +3,13 @@
 ## Description
 The purpose of this application is to ensure greater security for the client, their employees and the business data.
 
-The application will create generate strong and random passwords based on the criteria that they've selected. It runs in the browser and have a clean and polished, responsive user interface that adapts to multiple screen sizes.
+The password must be between 8 and 128 characters long and must contain one or more of the following characters:
+- Uppercase Letters: ABCDEFGHIJKLMNOPQRSTUVWXYZ
+- Lowercase Letters: abcdefghijklmnopqrstuvwxyz
+- Numbers: 0123456789
+- Special Characters: !@#$%^&*()+=[]{}<>/?!
+
+The application will generate strong and random passwords based on the criteria that the user has selected. It runs in the browser and have a clean and polished, responsive user interface that adapts to multiple screen sizes.
 
 ## Table of Contents
 - [Preview](#preview)
@@ -16,17 +22,28 @@ The application will create generate strong and random passwords based on the cr
 
 ## Usage
 
+You may use this application to provide more secure passwords for your online accounts by generating random passwords for you.
+
 ## License
 
 [MIT License](https://github.com/victoriamcn/Password-Generator/blob/main/LICENSE)
 
 ## Credits
 
-**Tutorial**
+**Special thanks to:**
+- [Tina Croxton](https://github.com/TinaTheDev91), who helped me figure out how to return a random generated password.
+- Rene Trevino, my tutor, who helped me think through how to structure my JavaScript code.
+
+**References:**
 - [Traversy Media: JavaScript Password Generator](https://www.youtube.com/watch?v=duNmhKgtcsI)
+- [Code Mystery: Random PAssword Generator Using JavaScript](https://dev.to/code_mystery/random-password-generator-using-javascript-6a)
+- [webdesign.tutsplus: Build a Configurable Random Password Generator With JavaScript](https://webdesign.tutsplus.com/tutorials/build-a-configurable-random-password-generator-with-javascript--cms-93262)
 
-**References**
-- [Charset Reference Document](http:www.net-comber.com/charset.html)
-
-
-## Tests
+**Tutorials:**
+- [.length](https://www.w3resource.com/javascript/form/string-length.php)
+- [How to get a number of random elements from an array?](https://stackoverflow.com/questions/19269545/how-to-get-a-number-of-random-elements-from-an-array)
+- [JavaScript Random](https://www.w3schools.com/js/js_random.asp)
+- [Password Returning Undefined](https://stackoverflow.com/questions/64378165/why-is-my-password-returning-as-undefined)
+- [Array.prototype.concat()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/concat)
+- [for loops in if statements](https://builtin.com/software-engineering-perspectives/can-you-put-a-for-loop-in-an-if-statement)
+- [EventTarget.addEventListener()](https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener)
--- script.js
+++ script.js
@@ -2,124 +2,125 @@
 
 //Button DOM Selector
 let generateBtn = document.querySelector("#generate");
-//User selection and confirm variables
-let input; //Variable for user's password length ~line 50
-let confirmUppercase; //made confirm for each character set so I could list all possible selections in my conditional statements ~line66
-let confirmLowercase;
-let confirmNumber;
-let confirmSpecialChar;
-let userSelection = []; //Variable to create a random password based on the userLength and confirmed characters
 
 //Uppercase Variable Array for userSelection
-let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 //console.log(specialChar)
 
 //Lowercase Variable Array for userSelection
-let lowerCase = ["a", "b", "c", "d", "e", "f", "j", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let lowerCase = ["a", "b", "c", "d", "e", "f", "j", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 //console.log(specialChar)
 
 //Number Variable Array for userSelection
-let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
 //console.log(number)
 
 //Function for Special Character Variable for userSelection
-let specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "=", "+", "[", "{", "]", "}", "<", ">", "/", "?"];
+let specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "=", "+", "[", "{", "]", "}", "<", ">", "/", "?"]
 //console.log(specialChar);
 
 // Write password to the #password input
 function writePassword() {
-  let password = createPassword();
-  let passwordText = document.querySelector("#password");
+  let password = generatePassword()
+  let passwordText = document.querySelector("#password")
   //tried created a new element to put the new password in and it didn't work//var elementP = document.createElement("p");//elementP.textContent = password;//console.log(elementP)
   //Password value into the text box
   passwordText.value = password;
 }
 
 // Generate button event listener for the mouse click
-generateBtn.addEventListener("click", writePassword); //debugger
+generateBtn.addEventListener("click", writePassword()); //debugger
 
 
 //Function to Generate PW
-function createPassword() {
-  while (true) {
-    var input = prompt("How long would you like your password to be? Please type a number between 8 and 128.");
-    if (!input) {
-      alert("Please type a number between 8 and 128.");
-    } else if (input < 8 || input > 128) {
-      prompt("Please type a number between 8 and 128.");
-    } else {
-      break
+function generatePassword() {
+
+    let passLength = prompt("How long would you like your password to be? Please type a number between 8 and 128.")
+    while ((passLength < 8) || (passLength > 128) || (isNaN(passLength)=== true)) {
+      alert("Please type a number between 8 and 128.")
+      passLength = prompt("Please type a number between 8 and 128.")
     }
+
+  //variables for confirming characters
+  let confirmUppercase = confirm("Would you like your password to have UPPERCASE letters?")
+  let confirmLowercase = confirm("would you like your password to have lowercase letters?")
+  let confirmNumber = confirm("Would you like your password to have numbers?")
+  let confirmSpecialChar = confirm("Would you like your password to contain special characters?: ! @ # $ % & * = + < > / ?")
+  let userSelection = [""] //empty array for character selection
+  let randomPassword = [""] //empty array for password generation
+  //console.log(randomPassword)
+
+  //Character Selection // tutor advised that .concat works best here over .push
+
+  //upperCase
+  if  (confirmUppercase === true) {
+    userSelection = userSelection.concat(upperCase)
   }
-  //confirming characters
-  confirmUppercase = confirm("Would you like your password to have UPPERCASE letters?");
-  confirmLowercase = confirm("would you like your password to have lowercase letters?");
-  confirmNumber = confirm("Would you like your password to have numbers?");
-  confirmSpecialChar = confirm("Would you like your password to contain special characters?: ! @ # $ % & * = + < > / ?");
-  
-  // User's Character Selection // advised .concat works best rather than push.
-  // All selections made
-  if (confirmUppercase && confirmLowercase && confirmNumber && confirmSpecialChar) {
-    userSelection.concat(upperCase, lowerCase, number, specialChar);
+  //lowerCase
+  if (confirmLowercase === true) {
+    userSelection = userSelection.concat(lowerCase)
+  }
+  //number
+  if (confirmNumber === true) {
+    userSelection = userSelection.concat(number)
+  }
+  //specialChar
+  if (confirmSpecialChar === true) {
+    userSelection = userSelection.concat(specialChar)
+  }
+
+  //function to randomize user's selections
+  function allRandom() {
+    return[Math.floor(Math.random() * passLength)]
+  }
+
+  //for loop selecting random characters based on chosen length
+  for (let i = 0; i < passLength; i++) {
+    //let allSelect = userSelection[Math.floor(Math.random() * userSelection.length)];
+    let allRandom = userSelection.sort(() => .5 - Math.random()).slice(0,i)
+    randomPassword.concat(allRandom);
+  }
+  console.log(randomPassword)
+  return randomPassword //.join("");
+  }
+
+//Below complicated code I worked on for several days before my classmates helped me make it simple
+    //(confirmUppercase && confirmLowercase && confirmNumber && confirmSpecialChar) {
+    //userSelection = userSelection.concat(upperCase, lowerCase, number, specialChar);
     //console.log(userSelection);
     // 3/4 Selections Made !upperCase
-  } else if (!confirmUppercase && confirmLowercase && confirmNumber && confirmSpecialChar) {
-    userSelection.concat(lowerCase, number, specialChar);
+  //} else if (!confirmUppercase && confirmLowercase && confirmNumber && confirmSpecialChar) {
+    //userSelection = userSelection.concat(lowerCase, number, specialChar);
     // 3/4 Selections Made !lowerCase
-  } else if (confirmUppercase && !confirmLowercase && confirmNumber && confirmSpecialChar) {
-    userSelection.concat(upperCase, number, specialChar);
+  //} else if (confirmUppercase && !confirmLowercase && confirmNumber && confirmSpecialChar) {
+    //userSelection = userSelection.concat(upperCase, number, specialChar);
     // 3/4 Selections Made !number
-  } else if (confirmUppercase && confirmLowercase && !confirmNumber && confirmSpecialChar) {
-    userSelection.concat(upperCase, lowerCase, specialChar);
+  //} else if (confirmUppercase && confirmLowercase && !confirmNumber && confirmSpecialChar) {
+    //userSelection = userSelection.concat(upperCase, lowerCase, specialChar);
     // 3/4 Selections Made !specialChar
-  } else if (confirmUppercase && confirmLowercase && confirmNumber && !confirmSpecialChar) {
-    userSelection.concat(upperCase, lowerCase, number);
+  //} else if (confirmUppercase && confirmLowercase && confirmNumber && !confirmSpecialChar) {
+    //userSelection = userSelection.concat(upperCase, lowerCase, number);
     // 2/4 Selections Made !upperCase !lowerCase
-  } else if (!confirmUppercase && !confirmLowercase && confirmNumber && confirmSpecialChar) {
-    userSelection.concat(number, specialChar);
+  //} else if (!confirmUppercase && !confirmLowercase && confirmNumber && confirmSpecialChar) {
+    //userSelection = userSelection.concat(number, specialChar);
     // 2/4 Selections Made !upperCase !number
-  } else if (!confirmUppercase && confirmLowercase && !confirmNumber && confirmSpecialChar) {
-    userSelection.concat(lowerCase, specialChar);
+  //} else if (!confirmUppercase && confirmLowercase && !confirmNumber && confirmSpecialChar) {
+   // userSelection = userSelection.concat(lowerCase, specialChar);
     // 2/4 Selections Made !upperCase !specialChar
-  } else if (!confirmUppercase && confirmLowercase && confirmNumber && !confirmSpecialChar) {
-    userSelection.concat(lowerCase, number);
+  //} else if (!confirmUppercase && confirmLowercase && confirmNumber && !confirmSpecialChar) {
+    //userSelection = userSelection.concat(lowerCase, number);
     // 2/4 Selections Made !lowerCase !number
-  } else if (confirmUppercase && !confirmLowercase && !confirmNumber && confirmSpecialChar) {
-    userSelection.concat(upperCase, specialChar);
+  //} else if (confirmUppercase && !confirmLowercase && !confirmNumber && confirmSpecialChar) {
+    //userSelection = userSelection.concat(upperCase, specialChar);
     // 2/4 Selections Made !lowerCase !specialChar
-  } else if (confirmUppercase && !confirmLowercase && confirmNumber && !confirmSpecialChar) {
-    userSelection.concat(upperCase, number);
+  //} else if (confirmUppercase && !confirmLowercase && confirmNumber && !confirmSpecialChar) {
+    //userSelection = userSelection.concat(upperCase, number);
     // 2/4 Selections Made !number !specialChar
-  } else if (confirmUppercase && confirmLowercase && !confirmNumber && !confirmSpecialChar) {
-    userSelection.concat(upperCase, lowerCase);
+  //} else if (confirmUppercase && confirmLowercase && !confirmNumber && !confirmSpecialChar) {
+    //userSelection = userSelection.concat(upperCase, lowerCase);
     // 1/4 Selections Made for upperCase only
     //don't need .concat here because it's only one confirm
-  } else if (confirmUppercase && !confirmLowercase && !confirmNumber && !confirmSpecialChar) {
-    userSelection = upperCase;
-    // 1/4 Selections Made for lowerCase only
-  } else if (!confirmUppercase && confirmLowercase && !confirmNumber && !confirmSpecialChar) {
-    userSelection = getRandomLower;
-    // 1/4 Selections Made for number only 
-  } else if (!confirmUppercase && !confirmLowercase && confirmNumber && !confirmSpecialChar) {
-    userSelection = getRandomNumber;
-    // 1/4 Selections Made for specialChar only
-  } else if (!confirmUppercase && !confirmLowercase && !confirmNumber && confirmSpecialChar) {
-    userSelection = specialChar;
-    // No selections made
-  };
-
   //function getRandomUpper(upperCase) { Math.floor(Math.random() * upperCase.length);}
   //function getRandomLower(lowerCase) { Math.floor(Math.random() * lowerCase.length);}
   //function getRandomNumber(number) {Math.floor(Math.random() * number.length);}
   //function getRandomSpecialChar(specialChar) { Math.floor(Math.random() * specialChar.length);}
-
-  //empty arry for new empyt password
-  var randomPassword = [];
-  console.log(randomPassword); //making an array, but index undefined.
-  //for loop selecting random characters based on chosen length
-  for (let i = 0; i < input; i++) {
-    let allSelections = userSelection[Math.floor(Math.random() * userSelection.length)];
-    randomPassword.push(allSelections);
-  } //separate getRandom functions didn't work, put the getRandom functions in the confirm instead.. .concat(getRandomUpper,getRandomLower,getRandomNumber,getRandomSpecialChar);
-  return randomPassword.join("");
-}
